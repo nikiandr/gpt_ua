6c9064a4bea34302592cf89e0e4bee1f322703fa
nikiandr <nikita.forduy@protonmail.com>
2023-08-13 18:11:15 +0200

    update data downloading script

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
-wget https://lang.org.ua/static/downloads/ubertext2.0/wikipedia/cleansed/ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2 -o data/ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2
-bzip2 -cd ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2 >> data/ubertext.wikipedia.filter_rus_gcld+short.text_only.txt
-rm ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2
-rm data/ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2
+mkdir -p data
+cd data || exit
+wget https://lang.org.ua/static/downloads/ubertext2.0/wikipedia/sentenced/ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2
+bzip2 -d ubertext.wikipedia.filter_rus_gcld+short.text_only.txt.bz2
